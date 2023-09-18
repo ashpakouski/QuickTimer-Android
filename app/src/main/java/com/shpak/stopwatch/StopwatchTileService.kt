4df35d2fd39d79c9abd3000592a3f6e4a013cc2b
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-09-18 16:58:40 +0300

    Add button to notification

--- app/src/main/java/com/shpak/stopwatch/StopwatchTileService.kt
+++ app/src/main/java/com/shpak/stopwatch/StopwatchTileService.kt
@@ -3,22 +3,63 @@ package com.shpak.stopwatch
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.Intent.CATEGORY_DEFAULT
 import android.content.Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.IntentFilter
 import android.net.Uri
 import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.service.quicksettings.TileService
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import java.util.Timer
 import java.util.TimerTask
 
 class StopwatchTileService : TileService() {
+    companion object {
+        private const val ACTION_START = "actionStart"
+        private const val ACTION_RESUME = "actionResume"
+        private const val ACTION_RESET = "actionReset"
+        private const val NOTIFICATION_ID = 0
+    }
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+
+    private val buttonClickReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == ACTION_START) {
+                var seconds = 0
+                Timer().scheduleAtFixedRate(object : TimerTask() {
+                    override fun run() {
+                        notificationBuilder.setContentTitle("$seconds")
+                        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+                        seconds++
+                    }
+                }, 0, 1000)
+            }
+        }
+    }
+
+    override fun onCreate() {
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        registerReceiver(buttonClickReceiver, IntentFilter(ACTION_START))
+        super.onCreate()
+    }
+
     override fun onClick() {
         super.onClick()
 
         showNotification()
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(buttonClickReceiver)
+        super.onDestroy()
     }
 
     private fun redirectToAppSettings() {
@@ -34,28 +75,29 @@ class StopwatchTileService : TileService() {
     }
 
     private fun showNotification() {
+        val startIntent = Intent(ACTION_START)
+        startIntent.action = ACTION_START
+        val startPendingIntent = PendingIntent.getBroadcast(
+            this, NOTIFICATION_ID,
+            startIntent, PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notificationChannel = NotificationChannel(
             "NOTIFICATION_CHANNEL_ID",
             "NOTIFICATION_CHANNEL_NAME",
             NotificationManager.IMPORTANCE_MIN
         )
 
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(notificationChannel)
 
-        val notification =
+        notificationBuilder =
             NotificationCompat.Builder(this, "NOTIFICATION_CHANNEL_ID").setOngoing(true)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setPriority(NotificationManager.IMPORTANCE_MAX)
                 .setCategory(Notification.CATEGORY_SERVICE)
+                .addAction(R.drawable.ic_launcher_foreground, "Start", startPendingIntent)
 
-        var seconds = 0
-        Timer().scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                notification.setContentTitle("$seconds")
-                notificationManager.notify(0, notification.build())
-                seconds++
-            }
-        }, 0, 1000)
+        notificationBuilder.setContentTitle("0")
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
     }
 }

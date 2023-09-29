7674853ee77b87e325e65103269a1c09cac6864b
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-09-29 18:07:38 +0300

    Refactor notification permission request dialog

--- app/src/main/java/com/shpak/quicktimer/dialog/NotificationPermissionRequestDialog.kt
+++ app/src/main/java/com/shpak/quicktimer/dialog/NotificationPermissionRequestDialog.kt
@@ -2,14 +2,30 @@ package com.shpak.quicktimer.dialog
 
 import android.app.AlertDialog
 import android.service.quicksettings.TileService
+import com.shpak.quicktimer.R
 
 object NotificationPermissionRequestDialog {
-    fun show(tileService: TileService) {
-        val builder = AlertDialog.Builder(tileService)
-        builder.setTitle("Attention")
-            .setMessage("Notifications permission is required for app to work")
-            .setPositiveButton("Grant in settings") { _, _ -> }
-            .setNegativeButton("Deny") { _, _ -> }
-        tileService.showDialog(builder.create())
+    fun show(
+        tileService: TileService,
+        onRequestGranted: (() -> Unit)? = null,
+        onRequestDenied: (() -> Unit)? = null
+    ) {
+        val dialog = AlertDialog.Builder(tileService)
+            .setIcon(R.drawable.ic_launcher_foreground)
+            .setMessage(
+                tileService.getString(
+                    R.string.notifications_permission_request_message,
+                    tileService.getString(R.string.app_name)
+                )
+            )
+            .setPositiveButton(tileService.getText(R.string.notification_permission_request_option_grant)) { _, _ ->
+                onRequestGranted?.invoke()
+            }
+            .setNegativeButton(R.string.notification_permission_request_option_deny) { dialog, _ ->
+                dialog.dismiss()
+                onRequestDenied?.invoke()
+            }
+            .create()
+        tileService.showDialog(dialog)
     }
 }

0932a9b7e89cd43639655ab2f6f7114142fbd9bb
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-10-08 12:38:50 +0300

    Register configuration change receiver

--- app/src/main/java/com/shpak/quicktimer/service/TimerTileService.kt
+++ app/src/main/java/com/shpak/quicktimer/service/TimerTileService.kt
@@ -1,12 +1,35 @@
 package com.shpak.quicktimer.service
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.service.quicksettings.TileService
+import android.util.Log
 import com.shpak.quicktimer.dialog.NotificationPermissionRequestDialog
 import com.shpak.quicktimer.dialog.TimerSettingsDialog
 import com.shpak.quicktimer.util.areNotificationsEnabled
 import com.shpak.quicktimer.util.redirectToAppSettings
+import java.util.Timer
+import java.util.TimerTask
 
 class TimerTileService : TileService() {
+
+    private val configurationChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        registerReceiver(
+            configurationChangeReceiver,
+            IntentFilter(Intent.ACTION_CONFIGURATION_CHANGED)
+        )
+    }
+
     override fun onClick() {
         super.onClick()
 
@@ -19,5 +42,11 @@ class TimerTileService : TileService() {
                     redirectToAppSettings(applicationContext)
                 })
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        unregisterReceiver(configurationChangeReceiver)
     }
 }
